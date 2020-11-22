7542dc80125a0a49982e7f527fafd43cd3409ddb
yusuebi <miro0512@gmail.com>
2020-11-22 17:59:08 +0900

    java学習セクション1~2

---
+++ section1/1-8/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    String concatPath(Stringfolder, Stringfile) {
+      if(!folder.endsWith("¥¥")) {
+        folder += "¥¥";
+      }
+      return folder + file
+    }
+  }
+}
